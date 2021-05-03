64b606ad976ae184ba200dfef2471a27397a60de
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-05-03 17:30:31 -0400

    test

--- src/pages/radio/RadioSpec.js
+++ src/pages/radio/RadioSpec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'wouter';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
